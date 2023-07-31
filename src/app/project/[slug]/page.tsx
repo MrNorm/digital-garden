4f2f61d2f1bf1beb59c2025e9b98d86c31e5f528
Oliver Northam <hi@oli.me>
2023-07-31 19:10:02 +0100

    Added sample slug for pulling projects via gql

---
+++ src/app/project/[slug]/page.tsx
@@ -0,0 +1,138 @@
+import React, { Fragment } from 'react';
+import styles from '../../page.module.css'
+import { getClient } from '@/lib/graphql-client'
+import escapeHTML from 'escape-html';
+import { Text } from 'slate'
+import { gql } from '@apollo/client'
+
+export const revalidate = 5
+const query = gql`query {
+  Projects(
+    where: { title: { contains: "Digital" } }
+    limit: 1
+  ) {
+    docs {
+      title,
+      tags { name },
+      publishedDate,
+      category { name },
+      content
+    }
+    totalDocs
+  }
+}`
+
+export default async function Home ({ params }: { params: { slug: string } }): Promise<JSX.Element> {
+  const client = getClient()
+  const { data } = await client.query({ query })
+  const project = data.Projects.docs[0] // TODO: Sort this out
+
+  return (
+    <main>
+        <h1>{project.title}</h1>
+        {serialize(project.content)}
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/project/[{params.slug}]/page.tsx</code>
+        </p>
+    </main>
+  )
+}
+
+const serialize = (children) => children.map((node, i) => {
+  if (Text.isText(node)) {
+    let text = <span dangerouslySetInnerHTML={{ __html: escapeHTML(node.text) }} />
+
+    if (node.bold) {
+      text = (
+        <strong key={i}>
+          {text}
+        </strong>
+      )
+    }
+
+    if (node.code) {
+      text = (
+        <code key={i}>
+          {text}
+        </code>
+      )
+    }
+
+    if (node.italic) {
+      text = (
+        <em key={i}>
+          {text}
+        </em>
+      )
+    }
+
+    // Handle other leaf types here...
+
+    return (
+      <Fragment key={i}>
+        {text}
+      </Fragment>
+    )
+  }
+
+  if (!node) {
+    return null
+  }
+
+  switch (node.type) {
+    case 'h1':
+      return (
+        <h1 key={i}>
+          {serialize(node.children)}
+        </h1>
+      )
+    // Iterate through all headings here...
+    case 'h6':
+      return (
+        <h6 key={i}>
+          {serialize(node.children)}
+        </h6>
+      )
+    case 'blockquote':
+      return (
+        <blockquote key={i}>
+          {serialize(node.children)}
+        </blockquote>
+      )
+    case 'ul':
+      return (
+        <ul key={i}>
+          {serialize(node.children)}
+        </ul>
+      )
+    case 'ol':
+      return (
+        <ol key={i}>
+          {serialize(node.children)}
+        </ol>
+      )
+    case 'li':
+      return (
+        <li key={i}>
+          {serialize(node.children)}
+        </li>
+      )
+    case 'link':
+      return (
+        <a
+          href={escapeHTML(node.url)}
+          key={i}
+        >
+          {serialize(node.children)}
+        </a>
+      )
+
+    default:
+      return (
+        <p key={i}>
+          {serialize(node.children)}
+        </p>
+      )
+  }
+})

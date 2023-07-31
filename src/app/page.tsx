1e59fc2dbe37172cf05321880561066a64b75b08
Oliver Northam <hi@oli.me>
2023-07-31 20:54:04 +0100

    Addressed type issues and escaping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,16 +26,16 @@ export default async function Home (): Promise<JSX.Element> {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Hello, my name is Oliver Northam and welcome to my space on the internet. From here you'll be able to learn about me, what projects I'm working on and generally what I'm getting up to in life.
+          Hello, my name is Oliver Northam and welcome to my space on the internet. From here you&lsquo;ll be able to learn about me, what projects I&lsquo;m working on and generally what I&lsquo;m getting up to in life.
         </p>
         <p>
           Established July 2023, this space is early days at the moment but I aim to contribute little and often to it as time goes on. Turning it from a dirt pile of unstyled text to a matured digital gardem.
         </p>
         <p>
-          For some insight on what I'm working on at the moment (including this site), check out the projects I have on below.
+          For some insight on what I&lsquo;m working on at the moment (including this site), check out the projects I have on below.
         </p>
         <ul>
-          {projects.map((project, index) => {
+          {projects.map(({ project, index }: { project: { slug: string, title: string }, index: number }) => {
             return (
               <li key={index}><Link href={`/project/${project.slug}`}>{project.title}</Link></li>
             )

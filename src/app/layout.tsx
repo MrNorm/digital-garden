6c9293432aca030729aaf480778227adfa7feaa5
Oliver Northam <hi@oli.me>
2023-08-02 07:33:14 +0100

    Playing with some styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { type Metadata } from 'next'
 import { Zilla_Slab } from 'next/font/google'
+import Link from 'next/link'
 
 const zilla = Zilla_Slab({ weight: ['300', '500', '600'], style: ['italic', 'normal'], subsets: ['latin'] })
 
@@ -16,7 +17,14 @@ export default function RootLayout ({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={zilla.className}>{children}</body>
+      <body className={zilla.className}>
+          <header>
+            <div className='logo'>
+              <Link href='/'>Oli<span className='dot'>.</span></Link>
+            </div>
+          </header>
+          {children}
+        </body>
     </html>
   )
 }

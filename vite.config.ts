5de4775f07458a8e733407228fa771f869bdefd1
MrNorm <hi@oli.me>
2024-09-17 19:25:32 +0200

    Actually lets go full static - Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import vercel from 'vite-plugin-vercel';
 import vike from 'vike/plugin'
+import { pages } from 'vike-cloudflare'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +11,6 @@ export default defineConfig({
   plugins: [
     react(), 
     vike({ prerender: true }),
-    vercel()
+    pages()
   ],
 })

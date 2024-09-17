29ba286d30dda7165ef43938bc7aed21a275374c
MrNorm <hi@oli.me>
2024-09-17 19:14:17 +0200

    Added Vercel stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vercel from 'vite-plugin-vercel';
 import vike from 'vike/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vike({ prerender: true })],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [
+    react(), 
+    vike({ prerender: true }),
+    vercel()
+  ],
 })

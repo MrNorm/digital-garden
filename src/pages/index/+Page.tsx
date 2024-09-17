edbe9cb8d5a2293b8742227a20e25831baf0cdfe
MrNorm <hi@oli.me>
2024-09-17 17:13:07 +0200

    Strip back examples into a min vite + vike setup with ssg

---
+++ src/pages/index/+Page.tsx
@@ -0,0 +1,13 @@
+export { Page }
+
+function Page() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      This page is:
+      <ul>
+        <li>Rendered to HTML.</li>      
+      </ul>
+    </>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 import vike from 'vike/plugin'
-import { UserConfig } from 'vite'
 
-const config: UserConfig = {
-  plugins: [react(), vike()]
-}
-
-export default config
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), vike({ prerender: true })],
+})
